1fd3bbd02bf5e41eabc03877b8124ef5b10ea4ed
Eric Dodgion <Barry.manowar@gmail.com>
2021-03-21 23:20:15 -0600

    commit 2

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+//Fetch 1  Current Weather
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+//Fetch 2 5 Day forcast
+//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+//Fetch 3 UV
+//http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
