3e828233943fbb3280856569729af964289c68fd
Eric Dodgion <Barry.manowar@gmail.com>
2021-03-22 00:00:16 -0600

    commit 3

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,43 @@
+var searchBtn = $(".searchBtn")
+
 //Fetch 1  Current Weather
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+var currentWeatherApi ="http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
 
+fetch(currentWeatherApi).then(function (response) {
+  if (response.ok) {
+    response.json().then(function (data) {
+      displayIssues(data);
+
+    }
+  });
+};
 //Fetch 2 5 Day forcast
-//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+var forcastApi = "http://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}";
 
+fetch(forcastApi).then(function (response) {
+  if (response.ok) {
+    response.json().then(function (data) {
+      displayIssues(data);
+
+    }
+  });
+};
 //Fetch 3 UV
-//http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
+var uvApi = "http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}"
+
+fetch(uvApi).then(function (response) {
+  if (response.ok) {
+    response.json().then(function (data) {
+      displayIssues(data);
+
+    }
+  });
+};
+
+
+
+      for(var i = 0; i < localStorage.lenth; i++) {
+        var city = localStorage.getItem(i);
+        var displayCity = $(".city").addclass(".cityDisplay")
+        displayCity.append("<li>" + city + "</li>")
+      };
