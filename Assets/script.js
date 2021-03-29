53cd39f23c01fe74f7dc09fecc05de8613d8877c
Eric Dodgion <Barry.manowar@gmail.com>
2021-03-28 22:45:48 -0600

    commit four

--- Assets/script.js
+++ Assets/script.js
@@ -1,102 +1,94 @@
 var input = document.querySelector("#enterCity");
 var button = document.querySelector("#searchBtn");
 
-
 var apiKey = "3d440218342a493796fb55b92661ecc1";
 
 for (var i = 0; i < localStorage.length; i++) {
-
   var city = localStorage.getItem(i);
-  // console.log(localStorage.getItem("City"));
   var cityName = $(".list-group").addClass("list-group-item");
-
   cityName.append("<li>" + city + "</li>");
 }
 
 var keyCount = 0;
 
-function handleSearchFormSubmit (event) {
-	event.preventDefault();
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
 
-var searchInputVal = document.querySelector('#enterCity').value;
+  var searchInputVal = document.querySelector("#enterCity").value;
 
-    if (!searchInputVal) {
-        window.alert('Please enter a City to search for...')
-    }
+  if (!searchInputVal) {
+    window.alert("Please enter a City to search for...");
+  }
 
-    getParams()
+  getParams();
 }
 
-function getParams (search){
-    var search = input.value;
-    console.log(search);
+function getParams(search) {
+  var search = input.value;
+  console.log(search);
 
-    getCurrentWeather(search);
-    getfiveDayForcast(search);
+  getCurrentWeather(search);
+  getfiveDayForcast(search);
 }
-
 
 function getCurrentWeather(search) {
-  var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + (search) + "&appid=" + (apiKey);
+  var urlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}&units=metric`;
 
-    fetch(urlCurrent)
+  fetch(urlCurrent)
     .then(function (response) {
-    console.log(response.ok);
-    var cityName = $(".list-group").addClass("list-group-item");
-    cityName.append("<li>" + input.value + "</li>");
-    localStorage.setItem(keyCount, input.value);
-    keyCount = keyCount + 1;
+      console.log(response.ok);
+      var cityName = $(".list-group").addClass("list-group-item");
+      cityName.append("<li>" + input.value + "</li>");
+      localStorage.setItem(keyCount, input.value);
+      keyCount = keyCount + 1;
 
-    if (!response.ok) {
-      throw response.json();
-}
-return response.json()
-
-  })
-
-  .then(function printWeatherResults(response) {
-    var currentWeather = $(".weatherResults").append("<div>").addClass(".card-body");
-    currentWeather.empty();
-    var weatherResults = currentWeather.append("<p>");
-    currentWeather.append(weatherResults);
-
-     
-            var time = new Date(response.dt * 1000);
-            weatherResults.append(response.name + " " + time.toLocaleDateString("en-US"));
-           
-            var temp = weatherResults.append("<p>");
-            
-            weatherResults.append(temp);
-            temp.append("<p>" + "Temperature: " + response.main.temp + "</p>");
-            temp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
-            temp.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
     })
 
+    .then(function printWeatherResults(response) {
+      var currentWeather = $(".weatherResults")
+        .addClass(".currentWeather")
+        .append("<div>");
+      currentWeather.empty();
+      var weatherResults = currentWeather.append("<p>");
+      currentWeather.append(weatherResults);
 
+      var time = new Date(response.dt * 1000);
+      weatherResults.append(
+        "<p>" + response.name + " " + time.toLocaleDateString("en-US") + "<p>"
+      );
+      var temp = weatherResults.append("<p>");
+      weatherResults.append(temp);
+      temp.append(
+        "<p>" + "Temperature: " + response.main.temp + " C°" + "</p>"
+      );
+      temp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
+      temp.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
+    });
 }
 
 function getfiveDayForcast(search) {
-  var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + (search) + "&appid=" + (apiKey);
+  var urlFiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${search}&appid=${apiKey}&units=metric`;
 
-    fetch(urlFiveDay)
+  fetch(urlFiveDay)
     .then(function (response) {
-    console.log(response.ok);
-    if (!response.ok) {
+      console.log(response.ok);
+      if (!response.ok) {
         throw response.json();
-}
+      }
 
-return response.json()
-})
-    .then(function printForecastResults(forecastResults){
+      return response.json();
+    })
+    .then(function printForecastResults(forecastResults) {
       console.log(forecastResults);
       var forecast = $(".5dayForecast").append("<div>").addClass(".card-body");
       forecast.empty();
       var currentForecast = forecast.append("<p>");
-    // .addClass("card-text");
-    forecast.append(currentForecast);
-      
-  
-    })
-  }
+      forecast.append(currentForecast);
+    });
+}
 
-  button.addEventListener('click', handleSearchFormSubmit);
+button.addEventListener("click", handleSearchFormSubmit);
